6d500f41d7f6bf82db81214a3cca9ab408f1b598
lelik26 <lelik.van-23@yandex.ru>
2025-04-13 11:57:13 +0800

    change bot_instance.py

--- app/bot_instance.py
+++ app/bot_instance.py
@@ -1,6 +1,6 @@
 import os
 from aiogram import Bot
-from config import TELEGRAM_BOT_TOKEN
+from app.config import TELEGRAM_BOT_TOKEN
 
 # Используем BOT_TOKEN из твоего config.py
 if not TELEGRAM_BOT_TOKEN:
